744ff9db427e58511852bc52438cc6edf014591e
Jerry25 <jerrybadilla@gmail.com>
2025-03-16 11:06:11 -0600

    add edit

--- src/app/dashboard/form/editpedido/form.jsx
+++ src/app/dashboard/form/editpedido/form.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React from "react";
+import { MdDeleteForever } from "react-icons/md";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { FaRegCheckCircle } from "react-icons/fa";
 
-export default function FormeditPedido({pedido}) {
+export default function FormeditPedido({ pedido }) {
   return (
-    <div>form</div>
-  )
+    <div className="container-90">
+      <div className="flex-column">
+        <div className="">
+          <p className="">
+            <strong>Cliente: {pedido.idCliente}</strong>
+          </p>
+          <p className="">
+            <strong>Nombre: {pedido.name}</strong>
+          </p>
+        </div>
+
+        <table className="">
+          <thead>
+            <tr>
+              <th>Codigo</th>
+              <th>Producto</th>
+              <th>Cantidad</th>
+              <th>Total</th>
+              <th></th>
+              <td><IoMdAddCircleOutline /></td>
+              
+            </tr>
+          </thead>
+          <tbody>
+            {pedido.productos.map((itm, index) => (
+              <tr key={index}>
+                <td>{itm.sku}</td>
+                <td>{itm.nombre}</td>
+                <td>{itm.cantidad}</td>
+                <td>
+                  {(itm.price * itm.cantidad).toLocaleString("es-ES", {
+                    style: "currency",
+                    currency: "CRC",
+                  })}
+                </td>
+                <td><MdDeleteForever/></td>
+                <td><FaRegCheckCircle /></td>
+                
+                
+              </tr>
+            ))}
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><MdDeleteForever/></td>
+            <td><FaRegCheckCircle /></td>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
--- src/app/dashboard/form/editpedido/page.jsx
+++ src/app/dashboard/form/editpedido/page.jsx
@@ -1,27 +1,43 @@
 
-import Search from '@/app/dashboard/form/editpedido/search'
-import Form from '@/app/dashboard/form/editpedido/form'
+import Search from '@/app/dashboard/form/editpedido/search';
+import Form from '@/app/dashboard/form/editpedido/form';
 import { Suspense } from "react";
-import './local.css'
-import {editPedido} from '@/app/dashboard/form/editpedido/actions'
+import './local.css';
+import { editPedido } from '@/app/dashboard/form/editpedido/actions';
+import { getUser } from '@/utils/dal';
 
 export default async function EditPedido({ searchParams }) {
-  const data = await searchParams;
-  const query = data?.query || "";
-  const pedido = await editPedido(query)
-  
-  
+  const query = searchParams?.query || "";
+  const Idpedido = await editPedido(query);
+  const usuario = await getUser();
+
+  console.log(usuario);
+  console.log(Idpedido);
+
+  if (!Idpedido) {
+    return (
+      <div>
+        <Search />
+        <h5>Pedido no encontrado</h5>
+      </div>
+    );
+  }
+
+  if (usuario.user !== Idpedido.vendedor) {
+    return (
+      <div>
+        <Search />
+        <h5>No tiene permiso para editar este pedido</h5>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <Search/>
-      <Suspense >
-        {
-          (pedido !== null)&&(
-            <Form pedido={pedido}/>
-          )
-        }
+      <Search />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <Form pedido={Idpedido} />
       </Suspense>
     </div>
-  )
+  );
 }
